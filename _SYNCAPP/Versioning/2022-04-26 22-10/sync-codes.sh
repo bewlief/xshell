2e03cc5060a233ff9c6f51819c541bc2ff68d9d1
bewlief <71935665@qq.com>
2022-04-26 22:22:30 +0800

    auto update: xjming @ Tue Apr 26 22:22:30     2022

--- _SYNCAPP/Versioning/2022-04-26 22-10/sync-codes.sh
+++ _SYNCAPP/Versioning/2022-04-26 22-10/sync-codes.sh
@@ -6,27 +6,23 @@ import string
 
 function update() {
     local repo="$1"
-    cd "$1" && git::au >/dev/null 2>&1
-    string::formatKeyValue "$repo" "updated"
+    cd "$1" && git::au
+    string::formatKeyValue "repo" "updated"
 }
 
 function main() {
-    local aexe="/c/Users/xjming/xsoft/system/Allway Sync/Bin/syncappw.exe"
     local code_root="$HOME/xcodes/xopensource"
     local gitee="$code_root/gitee"
     local github="$code_root/github"
-    local private_github="$code_root/private-github"
 
     ui::banner "sync codes to my open source repos"
-
-    start "$aexe -s xjenkins, xshell, github -e"
 
     update "$gitee/xjenkins"
     update "$gitee/xshell"
     update "$github/xjenkins"
     update "$github/xshell"
-    update "$private_github/xcodes"
-    update "$private_github/xops"
+    update "$github/xcodes"
+    update "$github/xops"
 
     ui::figlet "ALL DONE"
 }
