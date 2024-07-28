829c76c08983835c6f6d040b36954842f6cf117e
bewlief <71935665@qq.com>
2024-07-28 20:42:03 +0800

    @Cache example @ 2022-08-05 06:26:02

--- win/set-win-variables.bat
+++ win/set-win-variables.bat
@@ -6,6 +6,7 @@ setx -m "JAVA_HOME" "%USERPROFILE%\xsoft\dev\sdk\jdk\jdk17"
 setx -m "GRADLE_HOME" "%USERPROFILE%\xsoft\dev\build\gradle"
 setx -m "GRADLE_USER_HOME" "D:\Download\aaaa\app-temp\repos"
 setx -m "EXE4J_JAVA_HOME" %JAVA_HOME%
+setx -m "MYSQL_HOME" "%USERPROFILE%\xsoft\database\mariadb64"
 setx -m "GROOVY_HOME" "%USERPROFILE%\xsoft\dev\sdk\groovy"
 setx -m "GOROOT" "%USERPROFILE%\xsoft\dev\sdk\go"
 rem setx -m "GOPATH" "%USERPROFILE%\xcodes\mygo"
@@ -31,4 +32,4 @@ rem set git configuration
 
 set DEFAULT_PATH="C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Users\xjming\AppData\Local\Microsoft\WindowsApps"
 @REM set DEFAULT_PATH=""
-setx -m PATH "%JAVA_HOME%\bin;%GOROOT%\bin;%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%GIT_HOME%;%GRADLE_HOME%\bin;%GROOVY_HOME%\bin;%DEFAULT_PATH%";"%USERPROFILE%\xsoft\bin
+setx -m PATH "%JAVA_HOME%\bin;%MYSQL_HOME%\bin;%GOROOT%\bin;%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%GIT_HOME%;%GRADLE_HOME%\bin;%GROOVY_HOME%\bin;%DEFAULT_PATH%";%USERPROFILE%\xsoft\bin"
